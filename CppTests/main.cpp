55e768d9cfc2c382081588b49036b0bd2ce27717
Paula Hasstenteufel <paula.hasstenteufel@schibsted.se>
2018-03-14 14:15:30 +0100

    successfully executing with input

--- CppTests/main.cpp
+++ CppTests/main.cpp
@@ -8,8 +8,28 @@
 
 #include <iostream>
 
+double multiplyNumber(int number) {
+    return number * number;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int number;
+    
+    std::cout << "input a number to see it multiplied by itself:\n";
+    std::cin >> number;
+    std::cout << "\n";
+    
+    while(!std::cin) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
+        std::cout << "your input is not valid, please input a number:\n";
+        std::cin >> number;
+        std::cout << "\n";
+    }
+    
+    std::cout << "result of the operation is: " << multiplyNumber(number) << "\n";
+    
     return 0;
 }
